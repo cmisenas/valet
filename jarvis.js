9e3be4ca501336ec7ae833551c0e13ad39794cbd
cmisenas <acolyte.camille@gmail.com>
2013-10-03 02:13:07 -0400

    Initial working commit. Still buggy. TODO: Fix events for when showing the iframe

---
+++ jarvis.js
@@ -0,0 +1,102 @@
+;(function(){
+
+	// the minimum version of jQuery we want
+	var v = "1.10.2";
+
+	// check for jQuery. if it exists, verify it's not too old.
+	if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+		var done = false;
+		var script = document.createElement("script");
+		script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+		script.onload = script.onreadystatechange = function(){
+			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+				done = true;
+				initJarvis();
+			}
+		};
+		document.getElementsByTagName("head")[0].appendChild(script);
+	} else {
+		initJarvis();
+	}
+  
+	
+	function initJarvis() {
+		(window.jarvis = function() {
+      var keys = [],
+          mapping = {
+            71: "https://www.google.com/#q=%s", //g
+            87: "http://en.wikipedia.org/w/index.php?&search=%s", //w
+            65: "http://www.amazon.com/s/?field-keywords=%s", //a
+            69: "http://www.ebay.com/sch/i.html?_nkw=%s", //e
+            68: "http://dictionary.com/browse/%s", //d
+            84: "http://thesaurus.com/browse/%s", //t
+            89: "http://www.youtube.com/results?search_query=%s", //y
+            70: "http://www.facebook.com/search.php?q=%s", //f
+            77: "https://maps.google.com/?q=%s", //m
+          },
+          qs = '';
+
+			function getSelText() {
+				var s = '';
+				if (window.getSelection) {
+					s = window.getSelection();
+				} else if (document.getSelection) {
+					s = document.getSelection();
+				} else if (document.selection) {
+					s = document.selection.createRange().text;
+				}
+				return s;
+			}
+
+      $(document).on('keyup keydown', function(e) {
+        var s = getSelText();
+        if (s) {
+          
+          if (e.type === "keyup") {
+            delete keys[e.keyCode];
+          } else {
+            keys[e.keyCode] = true;
+          }
+
+          if (keys[17] && keys[16]) {
+            console.log(keys);
+            if (e.keyCode in mapping) {
+              qs = mapping[e.keyCode].replace(/\%s/g, encodeURI(s));
+              sendQuery(qs);
+            }
+          }
+        }
+      });
+
+      function sendQuery(qs) {
+        if ($("#wikiframe").length === 0) {
+          $("body").append("\
+          <div id='wikiframe'>\
+            <div id='wikiframe_veil' style=''>\
+              <p>Loading...</p>\
+            </div>\
+            <iframe src=" + qs + " onload=\"$('#wikiframe iframe').slideDown(500);\">Enable iFrames.</iframe>\
+            <style type='text/css'>\
+              #wikiframe_veil { display: none; position: fixed; width: 100%; height: 100%; top: 0; left: 0; background-color: rgba(255,255,255,.25); cursor: pointer; z-index: 900; }\
+              #wikiframe_veil p { color: black; font: normal normal bold 20px/20px Helvetica, sans-serif; position: absolute; top: 50%; left: 50%; width: 10em; margin: -10px auto 0 -5em; text-align: center; }\
+              #wikiframe iframe { display: none; position: fixed; top: 10%; left: 10%; width: 80%; height: 80%; z-index: 999; border: 10px solid rgba(0,0,0,.5); margin: -5px 0 0 -5px; }\
+            </style>\
+          </div>");
+          $("#wikiframe_veil").fadeIn(750);
+        } else {
+          $("#wikiframe_veil").fadeOut(750);
+          $("#wikiframe iframe").slideUp(500);
+          setTimeout("$('#wikiframe').remove()", 750);
+        }
+
+        $("#wikiframe_veil").click(function(event){
+          $("#wikiframe_veil").fadeOut(750);
+          $("#wikiframe iframe").slideUp(500);
+          setTimeout("$('#wikiframe').remove()", 750);
+        });
+      }
+      
+		})();
+	}
+
+})();
